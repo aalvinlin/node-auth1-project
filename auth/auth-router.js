8d67e18317a1063cb32fa0f326932fda75636088
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-24 16:09:22 -0800

    auth-router functions written

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,8 +1,52 @@
 const bcrypt = require("bcryptjs");
-
 const express = require("express");
 
+const database = require("../data/login.db3");
+
 const router = express.Router();
+
+// GET all users
+router.get("/", (req, res) => {
+
+    database.getUsers()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(error => {
+            res.status(500).json({message: "Could not get users."})
+        })
+})
+
+// GET a user by ID
+router.get("/:id", (req, res) => {
+
+    database.getUserByID(req.params.id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json({message: "Could not get user."})
+        })
+})
+
+
+// POST a user
+router.post("/", (req, res) => {
+
+    if (!req.body || !req.body.username || !req.body.password)
+        { res.status(400).json({message: "Username and password are both required."})}
+
+    else
+    {
+        database.addUser(req.body)
+            .then(usersAdded => {
+                res.status(200).json(usersAdded);
+            })
+            .catch(error => {
+                res.status(500).json({message: "Could not add user."})
+            })
+    }
+})
 
 module.exports = router;
 
